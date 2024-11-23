f3cacadc4badafe7e698523be6e17c68d3d10a3d
lucianamarques <lucianadacostamarques@gmail.com>
2024-11-23 09:19:06 -0800

    add first files

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+# lambda_function.py
+import boto3
+from datetime import date, datetime
+import logging
+import os
+import subprocess
+
+
+def lambda_handler(event, context):
+    log_file_name = f"masa_function_output-{date.year}-{date.month}-{datetime.hour}-{datetime.minute}-{datetime.second}.log"
+    logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    os.chdir("./")
+    command = "./function.sh"
+    r = subprocess.run(command.split(), capture_output=True)
+    logger.info(f'Executed run with return code: {r.returncode}')
+    s3 = boto3.client('s3')
+    s3.put_object(Body=log_file_name, Bucket='spot-pricing-research', Key=log_file_name)
+    return {
+        'statusCode': r.returncode,
+        'stdout': r.stdout,
+        'stderr': r.stderr,
+    }
