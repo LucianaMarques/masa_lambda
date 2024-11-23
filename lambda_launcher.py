533a823c15db79412c69e7085e1e4c7e8d843d09
lucianamarques <lucianadacostamarques@gmail.com>
2024-11-23 11:03:53 -0800

    revert lambda launcher

--- lambda_launcher.py
+++ lambda_launcher.py
@@ -1,23 +1,53 @@
-# lambda_function.py
 import boto3
-from datetime import date, datetime
-import logging
-import os
-import subprocess
+import json
+#import logging
 
 
-def lambda_handler(event, context):
-    log_file_name = f"masa_function_output-{date.year}-{date.month}-{datetime.hour}-{datetime.minute}-{datetime.second}.log"
-    logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
-    os.chdir("./")
-    command = "./function.sh"
-    r = subprocess.run(command.split(), capture_output=True)
-    logger.info(f'Executed run with return code: {r.returncode}')
-    s3 = boto3.client('s3')
-    s3.put_object(Body=log_file_name, Bucket='spot-pricing-research', Key=log_file_name)
-    return {
-        'statusCode': r.returncode,
-        'stdout': r.stdout,
-        'stderr': r.stderr,
-    }
+class LambdaLauncher():
+    def __init__(self):
+        print("Started Lambda Launcher")
+        #self.logger = logging.Logger() TODO: setup logger
+        self.client = boto3.client('lambda')
+
+
+    def register_function(self, function_name, runtime, role_arn, handler, zip_file_path):
+        print(f"Registering function with name: {function_name}")
+        with open(zip_file_path, 'rb') as f:
+            zipped_code = f.read()
+
+        # Register the Lambda function
+        response = self.client.create_function(
+            FunctionName=function_name,
+            Runtime=runtime,
+            Role=role_arn,
+            Handler=handler,
+            Code={
+                'ZipFile': zipped_code,
+            },
+            Description='Your function description',
+            Timeout=60,  # Timeout in seconds
+            MemorySize=128,  # Memory size in MB
+            Publish=True
+        )
+        print(f"Response: {response}")
+
+
+    def invoke_masa_function(self):
+        response = self.client.invoke(
+            FunctionName='masa-function',
+            InvocationType='RequestResponse',
+            LogType='Tail'
+        )
+        response_payload = response['Payload'].read().decode('utf-8')
+        print(response_payload)
+
+
+    def update_function_code(self, function_name, zip_file_path):
+        with open(zip_file_path, 'rb') as f:
+            zipped_code = f.read()
+
+        response = self.client.update_function_code(
+            FunctionName=function_name,
+            ZipFile=zipped_code,
+        )
+        print(response)
